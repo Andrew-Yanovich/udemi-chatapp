ed01144fa3adbae6e86824742b76e12cce48decf
Andrew <yanovi4.a@gmail.com>
2021-12-07 20:53:32 +0100

    318 - Retrieve data from firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -12,6 +13,18 @@ class ChatScreen extends StatelessWidget {
           padding: const EdgeInsets.all(8),
           child: Text('This works!'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance
+              .collection('chats/2L2vwdo60E9yw55m0hiY/messages')
+              .snapshots()
+              .listen((data) {
+            print(data.docs[0]['text']);
+            data.docs.forEach((doc) { print(doc['text']); });
+          });
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
