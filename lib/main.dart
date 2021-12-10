7144390275d2afa8ed7d4bba055005d0cd936b18
Andrew <yanovi4.a@gmail.com>
2021-12-10 18:45:02 +0100

    349 - Handling Push Notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:chatapp/screens/auth_screen.dart';
 import 'package:chatapp/screens/chat_screen.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   runApp(const MyApp());
 }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -25,6 +25,14 @@ class _ChatScreenState extends State<ChatScreen> {
       provisional: false,
       sound: true,
     );
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Got a message whilst in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
     super.initState();
   }
 
